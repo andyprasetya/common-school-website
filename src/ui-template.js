a04d7dd6442bf4d45c20bc6e17a684b6714a5855
Aditya-Kurniawan <masadhekurniawan123@gmail>
2019-10-24 11:26:52 +0700

    Test SignIn

--- src/ui-template.js
+++ src/ui-template.js
@@ -71,7 +71,7 @@ templates.topNavigation = function() {
           </ul>
           <form class='form-inline mt-2 mt-md-0'>
             <input class='form-control mr-sm-2' type='text' placeholder='Search' aria-label='Search'>
-            <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+            <a class='btn btn-outline-success my-2 my-sm-0' href='Sign In.html'>Sign In</a>
           </form>
         </div>
       </nav>
